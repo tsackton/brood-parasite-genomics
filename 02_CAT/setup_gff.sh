35d48a881f56322c245bfd54d574a2e3f338394e
Tim Sackton <tsackton@oeb.harvard.edu>
2018-02-22 16:06:19 -0500

    Updating GFF setup script

--- 02_CAT/setup_gff.sh
+++ 02_CAT/setup_gff.sh
@@ -12,5 +12,10 @@ grep "assembled-molecule" GCF_000002315.4_Gallus_gallus-5.0_assembly_report.txt 
 grep -v "assembled-molecule" GCF_000002315.4_Gallus_gallus-5.0_assembly_report.txt | grep -v "^#" | cut -f 5,7 >> chr_conv_table
 
 #translate
-gffread Gallus_gallus.Gallus_gallus-5.0.91.gff3 -m chr_conv_table -o galGal5-filtered.gff
+gffread Gallus_gallus.Gallus_gallus-5.0.91.gff3 -m chr_conv_table -o temp.gff
 
+#remove gffread header
+grep -v "^# " temp.gff > galGal5-filtered.gff
+
+#cleanup
+rm temp.gff
