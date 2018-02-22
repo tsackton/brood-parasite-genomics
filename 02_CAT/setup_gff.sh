a8ec9257f295b2bbdc90377ff61a256aa357511c
Tim Sackton <tsackton@g.harvard.edu>
2018-02-22 16:47:44 -0500

    Updating GFF code

--- 02_CAT/setup_gff.sh
+++ 02_CAT/setup_gff.sh
@@ -7,12 +7,13 @@ gunzip Gallus_gallus.Gallus_gallus-5.0.91.gff3.gz
 #get assembly report
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/315/GCF_000002315.4_Gallus_gallus-5.0/GCF_000002315.4_Gallus_gallus-5.0_assembly_report.txt
 
-#make chr translation table for gffread
+#make chr translation table for gffread, also removing short scaffolds
 grep "assembled-molecule" GCF_000002315.4_Gallus_gallus-5.0_assembly_report.txt | cut -f3,7 > chr_conv_table
-grep -v "assembled-molecule" GCF_000002315.4_Gallus_gallus-5.0_assembly_report.txt | grep -v "^#" | cut -f 5,7 >> chr_conv_table
+grep -v "assembled-molecule" GCF_000002315.4_Gallus_gallus-5.0_assembly_report.txt | grep -v "^#" |
+  awk 'BEGIN{OFS="\t"} {if ($10 > 1000) print $5, $7}' >> chr_conv_table
 
 #translate
-gffread Gallus_gallus.Gallus_gallus-5.0.91.gff3 -m chr_conv_table -o temp.gff
+gffread Gallus_gallus.Gallus_gallus-5.0.91.gff3 -m chr_conv_table -F -o temp.gff
 
 #remove gffread header
 grep -v "^# " temp.gff > galGal5-filtered.gff
